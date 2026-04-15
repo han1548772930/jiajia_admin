c7fece3ecbf13c0aad5d6374fecb4de7b4004fb0
hl <hl@nmcsoft.com>
2026-04-15 08:38:28 +0800

    fix(@vben/web-antdv-next): 更新web

--- apps/web-antdv-next/src/views/scheduled-tasks/components/TableActionButtons.vue
+++ apps/web-antdv-next/src/views/scheduled-tasks/components/TableActionButtons.vue
@@ -2,33 +2,46 @@
 import { Button } from 'antdv-next';
 import { IconifyIcon } from '@vben/icons';
 
+import type { TaskApi } from '#/api/task';
+
 defineOptions({
   name: 'TableActionButtons',
 });
-defineProps<{
+
+const props = defineProps<{
   params: any;
 }>();
+
+function getCurrentRowData(): TaskApi.JobData {
+  return props.params.node?.data ?? props.params.data;
+}
 </script>
 
 <template>
-  <div class="w-full h-full p-2 flex justify-start items-center flex-wrap gap-1">
-    <Button type="link" @click="params.onTableEdit(params.data)">
+  <div
+    class="w-full h-full p-2 flex justify-start items-center flex-wrap gap-1"
+  >
+    <Button type="link" @click="props.params.onTableEdit(getCurrentRowData())">
       <template #icon><IconifyIcon icon="lucide:edit" /></template>
       编辑
     </Button>
-    <Button type="link" @click="params.onTableCopy(params.data)">
+    <Button type="link" @click="props.params.onTableCopy(getCurrentRowData())">
       <template #icon><IconifyIcon icon="lucide:copy" /></template>
       复制
     </Button>
-    <Button type="link" @click="params.onTableRun(params.data)">
+    <Button type="link" @click="props.params.onTableRun(getCurrentRowData())">
       <template #icon><IconifyIcon icon="lucide:play" /></template>
       运行
     </Button>
-    <Button type="link" @click="params.onTableToLog(params.data)">
+    <Button type="link" @click="props.params.onTableToLog(getCurrentRowData())">
       <template #icon><IconifyIcon icon="lucide:file-text" /></template>
       日志
     </Button>
-    <Button type="link" danger @click="params.onTableDelete(params.data)">
+    <Button
+      type="link"
+      danger
+      @click="props.params.onTableDelete(getCurrentRowData())"
+    >
       <template #icon><IconifyIcon icon="lucide:trash-2" /></template>
       删除
     </Button>
